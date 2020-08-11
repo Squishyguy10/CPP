68f433571e4b3f8d13d933388927beb48aa57372
Squishyguy10 <squishyguy10@gmail.com>
2020-08-11 19:41:58 -0400

    first commit

---
+++ Fundamentals/test.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+    int TooncesKM = 60;
+    float HectorKM = TooncesKM + 15;
+    int hr = 60;
+
+    cout << "a) = " << 10 / (TooncesKM / hr) << "mins" << endl;
+    cout << "b) = " << 10 / (HectorKM / hr) << "mins" << endl;
+    cout << "c) = " << (10 / (TooncesKM / hr)) - (10 / (HectorKM / hr)) << "mins" << endl;
+
+    return 0;
+}
